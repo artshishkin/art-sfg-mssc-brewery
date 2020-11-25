80e4e7bdee84deb5a20459dde90f1cb80a490154
Art <d.art.shishkin@gmail.com>
2020-11-25 19:41:20 +0200

    GET Template (46. HTTP GET with Spring RestTemplate)

---
+++ brewery-client/src/main/java/net/shyshkin/study/breweryclient/web/client/BreweryClient.java
@@ -0,0 +1,30 @@
+package net.shyshkin.study.breweryclient.web.client;
+
+import lombok.Setter;
+import net.shyshkin.study.breweryclient.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "net.shyshkin", ignoreUnknownFields = false)
+public class BreweryClient {
+
+    public static final String BEER_PATH_V1 = "/api/v1/beer/";
+
+    @Setter
+    private String apihost;
+
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder builder) {
+        restTemplate = builder.build();
+    }
+
+    public BeerDto getBeerById(UUID beerId) {
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + beerId, BeerDto.class);
+    }
+}
