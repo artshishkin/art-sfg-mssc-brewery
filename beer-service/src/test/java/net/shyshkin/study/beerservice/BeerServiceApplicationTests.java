668ce6cd23b396268bb0065039eefde8a2002710
Art <d.art.shishkin@gmail.com>
2020-11-25 17:10:54 +0200

    40. LC Beer Service - Initial Spring Boot Project (made multi-module)

---
+++ beer-service/src/test/java/net/shyshkin/study/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
