44e6e29190a74ffd159a889c09ac17e0c7543130
Art <d.art.shishkin@gmail.com>
2020-12-01 16:04:00 +0200

    111. Custom Serializer with Jackson [skip ci]

--- brewery/src/main/java/net/shyshkin/study/artsfgmsscbrewery/web/model/v3/BeerDtoV3.java
+++ brewery/src/main/java/net/shyshkin/study/artsfgmsscbrewery/web/model/v3/BeerDtoV3.java
@@ -3,6 +3,7 @@ package net.shyshkin.study.artsfgmsscbrewery.web.model.v3;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -40,4 +42,7 @@ public class BeerDtoV3 {
     private OffsetDateTime createdDate;
     @Null
     private OffsetDateTime lastModifiedDate;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    LocalDate myLocalDate;
 }
