e2eddaf01bba128a333f937a59038587a77e51d1
Art <d.art.shishkin@gmail.com>
2020-12-02 19:51:58 +0200

    114. LC - Jackson JSON Creator - Example

--- beer-service/src/main/java/net/shyshkin/study/beerservice/web/model/BeerPagedList.java
+++ beer-service/src/main/java/net/shyshkin/study/beerservice/web/model/BeerPagedList.java
@@ -1,11 +1,29 @@
 package net.shyshkin.study.beerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public class BeerPagedList extends PageImpl<BeerDto> {
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerPagedList(
+            @JsonProperty("content") List<BeerDto> content,
+            @JsonProperty("number") int number,
+            @JsonProperty("size") int size,
+            @JsonProperty("totalElements") int totalElements,
+            @JsonProperty("pageable") JsonNode pageable,
+            @JsonProperty("totalPages") Long totalPages,
+            @JsonProperty("first") boolean first,
+            @JsonProperty("last") boolean last,
+            @JsonProperty("sort") JsonNode sort) {
+        super(content, PageRequest.of(number, size), totalElements);
+    }
 
     public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
         super(content, pageable, total);
