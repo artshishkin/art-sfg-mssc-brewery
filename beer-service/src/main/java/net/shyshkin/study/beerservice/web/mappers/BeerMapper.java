59f50274c64c49fcd0b87df02604ec112cee3136
Art <d.art.shishkin@gmail.com>
2020-12-03 18:35:06 +0200

    added BeerMapperDecorator (128. Enhance Get Beer with Inventory Information)

--- beer-service/src/main/java/net/shyshkin/study/beerservice/web/mappers/BeerMapper.java
+++ beer-service/src/main/java/net/shyshkin/study/beerservice/web/mappers/BeerMapper.java
@@ -2,11 +2,13 @@ package net.shyshkin.study.beerservice.web.mappers;
 
 import net.shyshkin.study.beerservice.domain.Beer;
 import net.shyshkin.study.beerservice.web.model.BeerDto;
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = DateMapper.class)
+@DecoratedWith(BeerMapperDecorator.class)
 public interface BeerMapper {
 
     BeerMapper INSTANCE = Mappers.getMapper( BeerMapper.class );
---
+++ beer-service/src/main/java/net/shyshkin/study/beerservice/web/mappers/BeerMapperDecorator.java
@@ -0,0 +1,31 @@
+package net.shyshkin.study.beerservice.web.mappers;
+
+import net.shyshkin.study.beerservice.domain.Beer;
+import net.shyshkin.study.beerservice.services.inventory.BeerInventoryService;
+import net.shyshkin.study.beerservice.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class BeerMapperDecorator implements BeerMapper {
+
+    private BeerInventoryService beerInventoryService;
+    private BeerMapper delegate;
+
+    @Autowired
+    public void setBeerInventoryService(BeerInventoryService beerInventoryService) {
+        this.beerInventoryService = beerInventoryService;
+    }
+
+    @Autowired
+    public void setDelegate(BeerMapper delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public BeerDto asBeerDto(Beer beer) {
+        BeerDto dto = delegate.asBeerDto(beer);
+
+        Integer onHand = beerInventoryService.getOnHandInventory(beer.getId());
+        dto.setQuantityOnHand(onHand);
+        return dto;
+    }
+}
