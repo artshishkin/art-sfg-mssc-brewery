96d419eda73b4f5d38e987a2c39aedc22dbe4e08
Art <d.art.shishkin@gmail.com>
2020-12-05 21:45:32 +0200

    Update Order Line Mapper to add data values. #2 (135. Assignment - Enhance Order Response with Beer Information)

--- beer-order-service/src/main/java/net/shyshkin/study/beerorderservice/web/mappers/BeerOrderLineMapper.java
+++ beer-order-service/src/main/java/net/shyshkin/study/beerorderservice/web/mappers/BeerOrderLineMapper.java
@@ -2,9 +2,11 @@ package net.shyshkin.study.beerorderservice.web.mappers;
 
 import net.shyshkin.study.beerorderservice.domain.BeerOrderLine;
 import net.shyshkin.study.beerorderservice.web.model.BeerOrderLineDto;
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 
 @Mapper(uses = {DateMapper.class})
+@DecoratedWith(BeerOrderLineMapperDecorator.class)
 public interface BeerOrderLineMapper {
     BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line);
 
