05c4271e7e8ff69006e7d416042e6bb838d3d9bc
Art <d.art.shishkin@gmail.com>
2020-12-08 20:54:44 +0200

    165. Assignment Review (SFG Impl) #9

--- beer-service/src/main/java/net/shyshkin/study/beerservice/bootstrap/BeerLoader.java
+++ beer-service/src/main/java/net/shyshkin/study/beerservice/bootstrap/BeerLoader.java
@@ -4,19 +4,23 @@ import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.beerservice.domain.Beer;
 import net.shyshkin.study.beerservice.repositories.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-//@Component
+@Component
 @RequiredArgsConstructor
 public class BeerLoader implements CommandLineRunner {
 
     private final BeerRepository beerRepository;
 
-    private static final String BEER_UPC_PATTERN = "06312342003%02d";
+    private final String[][] BEER_VALUES = {
+            {"Mango Bobs", "IPA", "0631234200036"},
+            {"Galaxy Cat", "PALE_ALE", "0631234300019"},
+            {"Pinball Porter", "PORTER", "0083783375213"}};
 
     @Override
     public void run(String... args) throws Exception {
@@ -25,24 +29,28 @@ public class BeerLoader implements CommandLineRunner {
     }
 
     private void bootstrapBeers() {
-        List<Beer> stubBeers = stubBeerList(9);
+        List<Beer> stubBeers = stubBeerList(3);
         beerRepository.saveAll(stubBeers);
     }
 
     public List<Beer> stubBeerList(int size) {
-        return IntStream.rangeClosed(1, size)
+        return IntStream.range(0, size)
                 .mapToObj(this::stubBeer)
                 .collect(Collectors.toList());
     }
 
     private Beer stubBeer(int index) {
+        String beerName = BEER_VALUES[index][0];
+        String beerStyle = BEER_VALUES[index][1];
+        String beerUPC = BEER_VALUES[index][2];
+
         return Beer.builder()
-                .beerName("BeerName" + index)
-                .beerStyle("BeerStyle" + index)
-                .price(BigDecimal.valueOf(111 * (index)))
-                .upc(String.format(BEER_UPC_PATTERN, index))
-                .minOnHand(111 * index)
-                .quantityToBrew(123 * index)
+                .beerName(beerName)
+                .beerStyle(beerStyle)
+                .price(new BigDecimal("12.95"))
+                .upc(beerUPC)
+                .minOnHand(12)
+                .quantityToBrew(200)
                 .build();
     }
 }
