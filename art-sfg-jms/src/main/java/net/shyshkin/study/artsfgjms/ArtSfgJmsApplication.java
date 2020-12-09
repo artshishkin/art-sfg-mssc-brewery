015dd527f5348301dfdadf4ae092bc4712348088
Art <d.art.shishkin@gmail.com>
2020-12-09 17:20:02 +0200

    172. Initial Project and Maven Dependencies (#11) Section 13: JMS Messaging

---
+++ art-sfg-jms/src/main/java/net/shyshkin/study/artsfgjms/ArtSfgJmsApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.artsfgjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSfgJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtSfgJmsApplication.class, args);
+    }
+
+}
---
+++ art-sfg-jms/src/test/java/net/shyshkin/study/artsfgjms/ArtSfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.artsfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtSfgJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
