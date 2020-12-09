9681c1a95bbdec177448cc0bbb5ad49941344abc
Art <d.art.shishkin@gmail.com>
2020-12-10 00:08:12 +0200

    179. Send and Receive of JMS Messages (My impl) (#11)

--- art-sfg-jms/src/main/java/net/shyshkin/study/artsfgjms/listener/HelloListener.java
+++ art-sfg-jms/src/main/java/net/shyshkin/study/artsfgjms/listener/HelloListener.java
@@ -5,13 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.artsfgjms.config.JmsConfig;
 import net.shyshkin.study.artsfgjms.model.HelloWorldMessage;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import javax.jms.JMSException;
 import javax.jms.Message;
 
 @Slf4j
@@ -19,30 +17,24 @@ import javax.jms.Message;
 @RequiredArgsConstructor
 public class HelloListener {
 
-    private final JmsTemplate jmsTemplate;
-
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
                        @Headers MessageHeaders headers,
                        Message message) {
-        log.debug("I got a message {}", helloWorldMessage);
-        log.debug("with headers {}", headers);
+//        log.debug("I got a message {}", helloWorldMessage);
+//        log.debug("with headers {}", headers);
 
 //        throw new RuntimeException("Testing jms_redelivered and JMSXDeliveryCount -> 6");
     }
 
     @JmsListener(destination = JmsConfig.SEND_RCV_QUEUE)
-    public void listenAndRespond(@Payload HelloWorldMessage helloWorldMessage,
-                                 @Headers MessageHeaders headers,
-                                 Message message) throws JMSException {
+    public HelloWorldMessage listenAndRespond(@Payload HelloWorldMessage helloWorldMessage) {
 
         String payloadString = helloWorldMessage.getMessage() + "_back";
 
-        HelloWorldMessage payloadMessage = HelloWorldMessage.builder()
+        return HelloWorldMessage.builder()
                 .message(payloadString)
                 .id(helloWorldMessage.getId())
                 .build();
-
-        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMessage);
     }
 }
