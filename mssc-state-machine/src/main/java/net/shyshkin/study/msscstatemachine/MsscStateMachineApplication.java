c2ae9a447ee68c2beb46be684526e22fccfef3b3
Art <d.art.shishkin@gmail.com>
2020-12-12 00:06:50 +0200

    201. Spring Boot Project Creation and Dependencies (#14) Section 15: Spring State Machine

---
+++ mssc-state-machine/src/main/java/net/shyshkin/study/msscstatemachine/MsscStateMachineApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.msscstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscStateMachineApplication.class, args);
+    }
+
+}
