7b82ceec1da184ba9d696caca9ec29999900211e
Art <d.art.shishkin@gmail.com>
2020-12-11 13:48:25 +0200

    fixed error when executing initialization SQL in MySQL DB. closes #13

--- beer-service/src/main/resources/data-mysql.sql
+++ beer-service/src/main/resources/data-mysql.sql
@@ -4,8 +4,8 @@ WHERE NOT EXISTS (select * FROM beer);
 
 INSERT INTO beer (id, beer_name, beer_style, created_date, last_modified_date, min_on_hand, quantity_to_brew, price, upc, version )
 SELECT 'a712d914-61ea-4623-8bd0-32c0f6545bfd', 'Galaxy Cat', 'PALE_ALE', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP , 12,  200, 12.95, '0631234300019', 1
-WHERE EXISTS (SELECT * FROM beer WHERE id = '0a818933-087d-47f2-ad83-2f986ed087eb');
+WHERE EXISTS (SELECT * FROM beer WHERE id = '0a818933-087d-47f2-ad83-2f986ed087eb') AND NOT EXISTS (SELECT * FROM beer WHERE id = 'a712d914-61ea-4623-8bd0-32c0f6545bfd');
 
 INSERT INTO beer (id, beer_name, beer_style, created_date, last_modified_date, min_on_hand, quantity_to_brew, price, upc, version )
 SELECT '026cc3c8-3a0c-4083-a05b-e908048c1b08', 'Pinball Porter', 'PORTER', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP , 12,  200, 12.95, '0083783375213', 1
-WHERE EXISTS (SELECT * FROM beer WHERE id = '0a818933-087d-47f2-ad83-2f986ed087eb');
+WHERE EXISTS (SELECT * FROM beer WHERE id = '0a818933-087d-47f2-ad83-2f986ed087eb') AND NOT EXISTS (SELECT * FROM beer WHERE id = '026cc3c8-3a0c-4083-a05b-e908048c1b08');
