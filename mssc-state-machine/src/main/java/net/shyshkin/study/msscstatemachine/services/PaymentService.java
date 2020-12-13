9c3271b84f31d6503ad5a54d54305a35dc4ff752
Art <d.art.shishkin@gmail.com>
2020-12-13 02:17:38 +0200

    207. Payment Service Creation (#14)

---
+++ mssc-state-machine/src/main/java/net/shyshkin/study/msscstatemachine/services/PaymentService.java
@@ -0,0 +1,18 @@
+package net.shyshkin.study.msscstatemachine.services;
+
+import net.shyshkin.study.msscstatemachine.domain.Payment;
+import net.shyshkin.study.msscstatemachine.domain.PaymentEvent;
+import net.shyshkin.study.msscstatemachine.domain.PaymentState;
+import org.springframework.statemachine.StateMachine;
+
+public interface PaymentService {
+
+    Payment newPayment(Payment payment);
+
+    StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId);
+
+}
---
+++ mssc-state-machine/src/main/java/net/shyshkin/study/msscstatemachine/services/PaymentServiceImpl.java
@@ -0,0 +1,41 @@
+package net.shyshkin.study.msscstatemachine.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.msscstatemachine.domain.Payment;
+import net.shyshkin.study.msscstatemachine.domain.PaymentEvent;
+import net.shyshkin.study.msscstatemachine.domain.PaymentState;
+import net.shyshkin.study.msscstatemachine.repository.PaymentRepository;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Override
+    public Payment newPayment(Payment payment) {
+        payment.setState(PaymentState.NEW);
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        return null;
+    }
+}
