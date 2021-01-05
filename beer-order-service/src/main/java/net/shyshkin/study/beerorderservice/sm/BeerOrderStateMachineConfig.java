d9c7a76502d66d3718c5d3f0cf45297d717a2842
Art <d.art.shishkin@gmail.com>
2021-01-05 18:16:57 +0200

    223. Configure State Machine States (#15)

---
+++ beer-order-service/src/main/java/net/shyshkin/study/beerorderservice/sm/BeerOrderStateMachineConfig.java
@@ -0,0 +1,27 @@
+package net.shyshkin.study.beerorderservice.sm;
+
+import net.shyshkin.study.beerorderservice.domain.BeerOrderEventEnum;
+import net.shyshkin.study.beerorderservice.domain.BeerOrderStatusEnum;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import static java.util.EnumSet.allOf;
+import static net.shyshkin.study.beerorderservice.domain.BeerOrderStatusEnum.*;
+
+@Configuration
+@EnableStateMachineFactory
+public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+    @Override
+    public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
+        states.withStates()
+                .initial(NEW)
+                .states(allOf(BeerOrderStatusEnum.class))
+                .end(DELIVERED)
+                .end(PICKED_UP)
+                .end(VALIDATION_EXCEPTION)
+                .end(ALLOCATION_EXCEPTION)
+                .end(DELIVERY_EXCEPTION);
+    }
+}
