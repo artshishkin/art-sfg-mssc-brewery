19df14fb8fc5ee3fc5513bea5370b8fb7934635b
Art <d.art.shishkin@gmail.com>
2021-01-11 12:21:38 +0200

    253. Spring Cloud Gateway Service Creation (#31 Section 19: Spring Cloud Gateway)

---
+++ art-sfg-mssc-brewery-gateway/src/main/java/net/shyshkin/study/artsfgmsscbrewerygateway/ArtSfgMsscBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.artsfgmsscbrewerygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSfgMsscBreweryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtSfgMsscBreweryGatewayApplication.class, args);
+    }
+
+}
