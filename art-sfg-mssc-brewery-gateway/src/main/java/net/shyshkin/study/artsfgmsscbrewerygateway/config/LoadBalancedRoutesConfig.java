ac25f99adc484d3176abfafc7124dee636176bd4
Art <d.art.shishkin@gmail.com>
2021-01-13 15:24:37 +0200

    264. Configure Gateway for Service Discovery (closes #35)

--- art-sfg-mssc-brewery-gateway/src/main/java/net/shyshkin/study/artsfgmsscbrewerygateway/config/LoadBalancedRoutesConfig.java
+++ art-sfg-mssc-brewery-gateway/src/main/java/net/shyshkin/study/artsfgmsscbrewerygateway/config/LoadBalancedRoutesConfig.java
@@ -1,6 +1,5 @@
 package net.shyshkin.study.artsfgmsscbrewerygateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,24 +7,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!local-discovery")
-public class LocalHostRouteConfig {
+@Profile("local-discovery")
+public class LoadBalancedRoutesConfig {
 
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder builder,
-                                        @Value("${net.shyshkin.routes.beer-service}") final String beerServiceUri,
-                                        @Value("${net.shyshkin.routes.beer-inventory-service}") final String beerInventoryServiceUri,
-                                        @Value("${net.shyshkin.routes.beer-order-service}") final String beerOrderServiceUri) {
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("beer-service",
                         r -> r.path("/api/v1/beer", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-                                .uri(beerServiceUri))
+                                .uri("lb://beer-service"))
                 .route("order-service",
                         r -> r.path("/api/v1/customers", "/api/v1/customers/**")
-                                .uri(beerOrderServiceUri))
+                                .uri("lb://order-service"))
                 .route("inventory-service",
                         r -> r.path("/api/v1/beer/*/inventory")
-                                .uri(beerInventoryServiceUri))
+                                .uri("lb://inventory-service"))
                 .build();
     }
 }
