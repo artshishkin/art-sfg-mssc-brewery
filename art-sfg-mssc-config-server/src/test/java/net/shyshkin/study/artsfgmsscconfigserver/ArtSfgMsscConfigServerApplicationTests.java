63ef3a12bf49e6a4a738b907796c7f55cfe204c3
Art <d.art.shishkin@gmail.com>
2021-01-15 00:22:00 +0200

    in test cloning git of config repo is disabled  (fixing issue #39)

--- art-sfg-mssc-config-server/src/test/java/net/shyshkin/study/artsfgmsscconfigserver/ArtSfgMsscConfigServerApplicationTests.java
+++ art-sfg-mssc-config-server/src/test/java/net/shyshkin/study/artsfgmsscconfigserver/ArtSfgMsscConfigServerApplicationTests.java
@@ -2,8 +2,12 @@ package net.shyshkin.study.artsfgmsscconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+        properties = {"spring.cloud.config.server.git.clone-on-start=false"}
+)
 class ArtSfgMsscConfigServerApplicationTests {
 
     @Test
