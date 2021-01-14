3965ce1b3fe839223704c49429fecd30275535a9
Art <d.art.shishkin@gmail.com>
2021-01-14 16:34:43 +0200

    272. Create Spring Cloud Config Server module (#38 Section 23: Spring Cloud Config)

---
+++ art-sfg-mssc-config-server/src/main/java/net/shyshkin/study/artsfgmsscconfigserver/ArtSfgMsscConfigServerApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.artsfgmsscconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSfgMsscConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtSfgMsscConfigServerApplication.class, args);
+    }
+
+}
