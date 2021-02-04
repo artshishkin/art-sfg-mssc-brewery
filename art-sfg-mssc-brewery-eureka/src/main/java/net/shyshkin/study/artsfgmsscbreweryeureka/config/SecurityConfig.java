18292324f30d31984d3eb61fe6c60aa849d8c16b
Art <d.art.shishkin@gmail.com>
2021-02-05 00:09:10 +0200

    Modified Security Settings of Eureka and Inventory Service to access health-check endpoint to all (#51, closes #54)

--- art-sfg-mssc-brewery-eureka/src/main/java/net/shyshkin/study/artsfgmsscbreweryeureka/config/SecurityConfig.java
+++ art-sfg-mssc-brewery-eureka/src/main/java/net/shyshkin/study/artsfgmsscbreweryeureka/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package net.shyshkin.study.artsfgmsscbreweryeureka.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,6 +12,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .authorizeRequests().antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
+                .and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
                 .httpBasic();
