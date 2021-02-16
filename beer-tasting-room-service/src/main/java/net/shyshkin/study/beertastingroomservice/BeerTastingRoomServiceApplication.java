fc23c1dbb68bc63db3931cfb4e59dcfecded85f8
Art <d.art.shishkin@gmail.com>
2021-02-16 20:51:59 +0200

    Created module Tasting Room Service (#67 Tasting Room Challenge) [skip ci]

---
+++ beer-tasting-room-service/src/main/java/net/shyshkin/study/beertastingroomservice/BeerTastingRoomServiceApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.beertastingroomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerTastingRoomServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerTastingRoomServiceApplication.class, args);
+    }
+
+}
---
+++ beer-tasting-room-service/src/test/java/net/shyshkin/study/beertastingroomservice/BeerTastingRoomServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.beertastingroomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerTastingRoomServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
